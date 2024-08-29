236e7ff0441858d99da646e20dca6b94a812199a
javedalihosamani <javed.hosamani007@gmail.com>
2024-08-29 17:33:12 +0530

    React Router and Layout completed

---
+++ src/components/Ex1.jsx
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'
+
+const Ex1 = () => {
+  return (
+    <Fragment>
+        <div className="container my-3">
+            <div className="row">
+                <div className="col text-center">
+                    <h1 className="display-4 text-success">useState Hook</h1>
+                    <p className="text-secondary">
+                        THAT ALLOWS YOU TO HAVE A STATE VARIABLES IN FUNCTIONAL COMPONENTS.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </Fragment>
+  )
+}
+
+export default Ex1;
---
+++ src/layout/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-success'>
+        <div className="container">
+            <div className="row">
+                <div className="col p-">
+                    <p className="text-white text-center">All Rights Reserved by Javed</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
